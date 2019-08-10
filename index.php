6b86fcff395066ab20e9da498a6b5e45fbf7b1c0
kurma-dev <53960125+kurma-dev@users.noreply.github.com>
2019-08-10 17:14:34 +0700

    Side-bar-responsive

---
+++ index.php
@@ -0,0 +1,35 @@
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="style.css">
+    <script src="https://kit.fontawesome.com/251feaa97c.js"></script>
+    <title>Menu Responsive</title>
+</head>
+
+<body>
+    <div class="middle">
+        <div class="menu">
+            <li class="item" id="profile">
+                <a href="#profile" class="btn"><i class="fab fa-accusoft"></i>Account</a>
+                <div class="smenu">
+                    <a href="#">Services</a>
+                    <a href="#">Login</a>
+                    <a href="#">SignUp</a>
+                </div>
+            </li>
+            <li class="item" id="settings">
+                <a href="#settings" class="btn"><i class="fas fa-cogs"></i>Setting</a>
+                <div class="smenu">
+                    <a href="#">About</a>
+                    <a href="#">Create Account</a>
+                </div>
+            </li>
+        </div>
+    </div>
+
+</body>
+
+</html>
